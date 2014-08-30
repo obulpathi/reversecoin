20b9484d522ee93376e430038b3f219fda5afc82
Obulapathi N Challa <obulpathi@gmail.com>
2014-08-29 20:27:30 -0400

    Adds withdraw and fastwithdraw tests

---
+++ timevault/wallet/test_withdraw.py
@@ -0,0 +1,12 @@
+import time
+from vault import Vault
+
+vault = Vault()
+
+# send coins to vault
+while vault.send():
+    time.sleep(1)
+
+# withdraw coins from vault
+while vault.withdraw():
+    time.sleep(1)
