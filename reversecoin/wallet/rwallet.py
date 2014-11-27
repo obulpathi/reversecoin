0a95ecaf66146733d38ab722dd26160e4095a5b2
Obulapathi N Challa <obulpathi@gmail.com>
2014-11-27 10:07:47 -0500

    Adds error handling to client

--- reversecoin/wallet/rwallet.py
+++ reversecoin/wallet/rwallet.py
@@ -7,108 +7,114 @@ from reversecoin.bitcoinrpc.exceptions import TransportException
 from reversecoin.version import VERSION, COPYRIGHT_YEAR
 
 def info(wallet):
-
     info = wallet.getinfo()
-    print "Blocks:", info.blocks
+    print("Blocks: {0}".format(info.blocks))
 
 def newaddress(wallet):
-
     address = wallet.getnewaddress()
     print address
 
 def account(wallet):
-
     account = wallet.getaccount()
-
+    if not account:
+        print("No accounts created yet!")
     for subaccount in account.itervalues():
-        print "Address: ", subaccount['address']
-        print "Public key: ", subaccount['public_key']
-        print "Private key: ", subaccount['private_key']
-        print "Balance: ", subaccount['balance']
-        print "\n\n"
+        print("Address: {0}".format(subaccount['address']))
+        print("Public key: {0}".format(subaccount['public_key']))
+        print("Private key: {0}".format(subaccount['private_key']))
+        print("Balance: {0}".format(subaccount['balance']))
+        print("\n\n")
 
 def balance(wallet):
-
     account = wallet.getaccount()
-
-    print ('\nAccounts')
+    print("\nAccounts")
+    if not account:
+        print("\tNo accounts created yet!")
     for subaccount in account.itervalues():
         print subaccount['address']  + ": ", subaccount['balance']
 
-    print ('\nVaults')
+    print("\nVaults")
     vaults = wallet.getvaults()
+    if not vaults:
+        print("\tNo vaults created yet!")
     for vault in vaults.itervalues():
-        print vault['name']  + ": ", vault['balance']
+        print vault["name"]  + ": ", vault["balance"]
 
-    print ('\nPending Transfers')
     transactions = wallet.getpendingtransactions()
+    print("\nPending Transfers")
+    if not transactions:
+        print("\tNo pending trsactions!")
     for transaction in transactions.itervalues():
-        fromaddress = transaction['inputs'][0]
-        for txouts in transaction['outputs']:
-            print fromaddress, "->", txouts['toaddress'] + ": ", txouts['amount']
+        fromaddress = transaction["inputs"][0]
+        for txouts in transaction["outputs"]:
+            print fromaddress, "->", txouts["toaddress"] + ": ", txouts["amount"]
 
-def getvaults(wallet):
-
+def vault_info(wallet):
     vaults = wallet.getvaults()
+    print("Vaults")
+    if not vaults:
+        print("\tNo vaults created yet!")
     for vault in vaults:
-        print "Vault Address:", vault
-        print "\tBalance: ", vaults[vault]['balance']
-        print "\tAddress:", vaults[vault]['address']
-        print "\tMaster Address:", vaults[vault]['master_address']
-        print "\ttimeout:", vaults[vault]['timeout']
-        print "\tReceived transactions:\n",
+        print("Vault Address: {0}".format(vault))
+        print("\tBalance: {0}".format(vaults[vault]['balance']))
+        print("\tAddress: {0}".format(vaults[vault]['address']))
+        print("\tMaster Address: {0}".format(vaults[vault]['master_address']))
+        print("\ttimeout: {0}".format(vaults[vault]['timeout']))
+        print("\tReceived transactions:")
         if vaults[vault]['received']:
-            print '\t\ttxhash:', vaults[vault]['received']['txhash'],
-            print 'n:', vaults[vault]['received']['n'], 'value:', vaults[vault]['received']['value']
+            print("\t\ttxhash: {0}".format(vaults[vault]['received']['txhash']))
+            print("n: {0}, value: {1}".format(
+                vaults[vault]['received']['n'],
+                vaults[vault]['received']['value']))
         else:
-                print "\t\tNone"
+                print("\t\tNone")
 
 def blockchain(wallet):
-
     wallet.dumpblockchain()
 
 def mempool(wallet):
-
     wallet.dumpmempool()
 
 def received(wallet):
-
     address = raw_input("Enter the address to check received transactions: ")
     txouts = wallet.received(address)
+    if not txouts:
+        print("This address did not receive any transactions!")
     for count, txhash in enumerate(txouts):
-        print count
-        print "\ttxhash: ", txouts[txhash]['txhash']
-        print "\tn: ", txouts[txhash]['n']
-        print "\tvalue: ", txouts[txhash]['value']
+        print(count)
+        print("\ttxhash: {0}".format(txouts[txhash]['txhash']))
+        print("\tn: {0}".format(txouts[txhash]['n']))
+        print("\tvalue: {0}".format(txouts[txhash]['value']))
 
 def send(wallet):
-
     account = wallet.getaccount()
-
     toaddress = raw_input("Enter the address to send coins to: ")
-    amount = int(input("Enter the balance to transfer to address: "))
-
+    try:
+        amount = int(input("Enter the balance to transfer to address: "))
+    except Exception as e:
+        print("Please enter a valid amount.")
+        exit(1)
+    if amount < 1:
+        print("Please enter a valid amount.")
+        exit(2)
     balance = 0
     for subaccount in account.itervalues():
         balance = balance + subaccount['balance']
-
     if balance < amount:
         print("Not enough balance")
-        exit(1)
-
-    print "Transferring: ", amount, " \tto: ", toaddress
+        exit(3)
+    print("Transferring: {0} to: {1}".format(amount, toaddress))
     wallet.connection.sendtoaddress(toaddress, amount)
 
-def savings(wallet):
-
+def vault_balance(wallet):
     vaults = wallet.getvaults()
+    if not vaults:
+        print("No vaults created!")
     for vault in vaults:
-        print vault  + ": ", vaults[vault]['balance']
+        print("{0}: {1}".format(vault, vaults[vault]['balance']))
 
 def vault_new(wallet):
-
     account = wallet.getaccount()
-
     toaddress = wallet.getnewaddress()
     tomaster_address = wallet.getnewaddress()
     timeout = 20
@@ -124,9 +130,7 @@ def vault_new(wallet):
         print("An error occured while creating vault")
 
 def vault_send(wallet):
-
     account = wallet.getaccount()
-
     balance = 0
     for subaccount in account.itervalues():
         balance = balance + subaccount['balance']
@@ -138,38 +142,60 @@ def vault_send(wallet):
             emptyvaults[vault] = vaults[vault]
 
     vaults = emptyvaults
+    if not vaults:
+        print("No empty vaults, please create one!")
+        exit(1)
+
     vault_names = [vault for vault in vaults]
     for count, vault in enumerate(vault_names):
-        print count, ":", vault + " ", vaults[vault]['balance']
+        print("{0}: {1}".format(count, vault))
 
-    choice = int(input("Select the vault to transfer money to: "))
+    try:
+        choice = int(input("Select the vault to transfer money to: "))
+    except:
+        print("Please enter valid choice!")
+        exit(2)
     if choice < 0 or choice > len(vaults)-1:
         print("Invalid choice")
-        exit(1)
+        exit(3)
 
     vault_address = vault_names[choice]
 
-    amount = int(input("Enter the balance to transfer to vault: "))
+    try:
+        amount = int(input("Enter the balance to transfer to vault: "))
+    except:
+        print("Please enter valid amount.")
+        exit(4)
+
+    if amount < 1:
+        print("Please enter valid amount.")
+        exit(5)
 
     if balance < amount:
         print("Not enough balance")
-        exit(1)
+        exit(6)
 
-    print("Transfering %d to vault %s" % (amount, vault_address))
+    print("Transfering %d to vault %s".format(amount, vault_address))
     ret_value = wallet.sendtovault(vault_address, amount)
     if not ret_value:
         print("An error occured while trasfering")
 
 def vault_withdraw(wallet):
-
     account = wallet.getaccount()
     toaddress = wallet.getnewaddress()
 
-    print("Available vaults")
     vaults = wallet.getvaults()
+    vaults = [vault for vault in vaults if vault['balance']]
+
+    if not vautls:
+        print("No vaults")
+
+    print("Available vaults")
     for n, vault in enumerate(vaults.itervalues()):
-        print "Id: ", n, vault['name']  + ": ", vault['balance']
-    index = int(input("Enter the id of the vault you want to transfer balance from: "))
+        print("Id: {0}, Address: {1}, Balance: {2}".format(
+            n, vault["name"]  + ": ", vault["balance"]))
+
+    index = int(input("Enter the id of the vault you want to transfer coins from: "))
 
     for n, vault in enumerate(vaults.itervalues()):
         if index == n:
@@ -253,16 +279,16 @@ def run(args):
 _SUPPORTED_COMMANDS = [
     ("account", "Look at the account summary.",),
     ("balance", "Current wallet balance.",),
-    ("getvaults", "Detailed information about the vaults.",),
     ("blockchain", "Dump the current block chain.",),
     ("info", "Get basic info",),
     ("mempool", "Dump the mempool.",),
     ("newaddress", "Generate a new address.",),
     ("received", "Received transactions.",),
     ("send", "Send coins to normal address.",),
-    ("savings", "Balance in each vault account.",),
+    ("vault_info", "Detailed information about the vaults.",),
     ("vault_new", "Create a new vault account"),
     ("vault_send", "Send to a vault.",),
+    ("vault_balance", "Balance in each vault account."),
     ("vault_withdraw", "Withdraw from a vault.",),
     ("vault_override", "Override a vault transaction.",),
     ("vault_fast_withdraw", "Withdraw from a vault immediately. No timeout associated.",),
